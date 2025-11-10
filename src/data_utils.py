e764175680066da39e0af2b9addd11fe11240700
Yajat Malik <yajatmalikofficial@gmail.com>
2025-11-10 06:40:49 +0530

    Updates

--- src/data_utils.py
+++ src/data_utils.py
@@ -10,17 +10,3 @@ def load_breast_cancer_df():
     df = pd.concat([b.data, b.target.rename("target")], axis=1)
     return df
 
-def load_titanic_csv(path="data/raw/titanic.csv"):
-    if not os.path.exists(path):
-        raise FileNotFoundError(f"Titanic CSV not found at {path}. Put data in data/raw/")
-    return pd.read_csv(path)
-
-def load_pima_csv(path="data/raw/pima_diabetes.csv"):
-    if not os.path.exists(path):
-        raise FileNotFoundError(f"Pima CSV not found at {path}. Put data in data/raw/")
-    return pd.read_csv(path)
-
-def load_bank_marketing_csv(path="data/raw/bank-additional/bank-additional-full.csv"):
-    if not os.path.exists(path):
-        raise FileNotFoundError(f"Bank Marketing CSV not found at {path}. Put data in data/raw/")
-    return pd.read_csv(path, sep=';')
